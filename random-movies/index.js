391ad7bb247a7d7e09a271cacad0a1e2f436f724
Martina Cesca <marticesca25@gmail.com>
2024-11-02 11:50:08 -0300

    Empecé a implementar el microservicio Random-movies. Falta prueba con movies

---
+++ random-movies/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+const PORT = 3001;
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10) + 1; // Número aleatorio entre 1 y 10
+}
+
+// Lee el archivo JSON y devuelve n películas aleatorias
+app.get("/api/movies", async (req, res) => {
+  const n = getRandomNumber();
+
+  try {
+    // Consumimos el microservicio GET /api/random/{n}
+    const response = await axios.get(`http://localhost:3000/api/random/${n}`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching movies." });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Microservicio [RANDOM-MOVIES] escuchando en http://localhost:${PORT}`);
+});
